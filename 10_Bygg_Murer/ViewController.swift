7a80297e9d9c1b6e1518490b32af576cbd70245e
marielaulie <54407324+marielaulie@users.noreply.github.com>
2021-06-15 14:52:52 +0200

    Oppstart app

---
+++ 10_Bygg_Murer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  10_Bygg_Murer
+//
+//  Created by Mariel Aulie Hinderaker on 15/06/2021.
+//  Copyright © 2021 Mariel Aulie Hinderaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
